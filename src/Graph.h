95cf62603abebb644e5129f778482ed5c577860d
Adi_tya21 <100029538+Aditya08p@users.noreply.github.com>
2024-11-26 00:13:34 +0530

    Create Graph.h

---
+++ src/Graph.h
@@ -0,0 +1,33 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <queue>
+#include <limits>
+
+class GraphNode {
+public:
+    std::string name;
+    std::string identity;
+    int island_number;
+
+    GraphNode();
+};
+
+class Graph {
+private:
+    std::vector<GraphNode> nodes;
+    std::vector<std::vector<int>> adj_list;
+    std::vector<int> islands;
+
+public:
+    Graph(int numNodes, int numIslands, int islandID);
+    void addNode();
+    int dijkstraShortestPath(int source, int destination, std::vector<std::string>& path);
+    int calculateShortestDistance(const std::vector<Graph>& islands, const std::string& src, const std::string& dest, std::vector<std::string>& path);
+    void printGraph();
+};
+
+#endif
